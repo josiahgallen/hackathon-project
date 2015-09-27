b32390c2e6cd1c1d135d08000f86ec5f4e4e5789
Leslie Sutton <l4sutton@gmail.com>
2015-09-27 13:38:24 -0500

    styling working with js. attempting to udpate with just scss

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
             $profile.show();
             $searchForm.hide();
             $('#hpCards').hide();
+            $('.heroTxt').text('');
             $('.heroImg').css('height', '10em');
             $('#view-all').hide();
         },
